deaa84091698a79276d1625c8424bc78c9de6c0d
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-25 20:52:00 +0100

    day 34 challenge progress

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,12 +6,21 @@ class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
-        self.score = 0
+        self.score_brain = 0
         self.question_list = q_list
         self.current_question = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
+
+    def first_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        q_text = html.unescape(self.current_question.text)
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        user_question = f"Q.{self.question_number}: {q_text} (True/False): "
+        # self.check_answer()  # I need to pass the answer to this function
+        return '\n'.join(wrap(user_question, width=25))
 
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
@@ -19,16 +28,17 @@ class QuizBrain:
         q_text = html.unescape(self.current_question.text)
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         user_question = f"Q.{self.question_number}: {q_text} (True/False): "
-        # self.check_answer(user_answer)
-        return '\n'.join(wrap(user_question, width=30))
+        # self.check_answer()  # I need to pass the answer to this function
+        return '\n'.join(wrap(user_question, width=25))
 
     def check_answer(self, user_answer):
+        score = 0
         correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
+        if user_answer == correct_answer:
+            self.score_brain += 1
+            return f"You got it right! Your current score is: {self.score_brain}/{self.question_number}"
         else:
-            print("That's wrong.")
+            return f"That's wrong. Your current score is: {self.score_brain}/{self.question_number}"
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        # print(f"Your current score is: {self.score}/{self.question_number}")
+        # print("\n")
--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
 from quiz_brain import QuizBrain
-
+import time
 THEME_COLOR = "#375362"
 
 
@@ -11,7 +11,8 @@ class QuizInterface(QuizBrain):
     def __init__(self, q_list):
         super().__init__(q_list)
         self.window = Tk()
-
+        self.answer = None
+        self.score_value = 0
         self.window.title("Quizzler")
         self.window.geometry("350x500")
         # PIL start
@@ -25,7 +26,7 @@ class QuizInterface(QuizBrain):
         self.window.config(bg=THEME_COLOR, padx=20, pady=40)
 
         self.canvas = Canvas(self.window, width=300, height=250)
-        self.canvas.create_text(150, 50, text=self.next_question(), font='Helvetica 15 bold', fill="black")
+        self.canvas.create_text(150, 90, text=self.first_question(), font='Helvetica 15 bold', fill="black")
         # self.canvas.create_text(120, 50, text=self.next_question(), font='Helvetica 15 bold', fill="black")
 
         self.canvas.grid(column=0, row=1, columnspan=2)
@@ -33,18 +34,22 @@ class QuizInterface(QuizBrain):
         self.score = tkinter.Label(text="Score: 0", bg=THEME_COLOR, fg="white", anchor=CENTER)
         self.score.grid(column=1, row=0, sticky=SE)
 
-        self.green_button = tkinter.Button(image=self.new_green, border=0, borderwidth=0, background="green")
+        self.green_button = tkinter.Button(image=self.new_green, border=0, borderwidth=0, background="green", command=lambda: self.user_answer("True"))
         self.green_button.place(x=40, y=305)
 
-        self.red_button = tkinter.Button(image=self.new_red, border=0, background="red", anchor=S)
+        self.red_button = tkinter.Button(image=self.new_red, border=0, background="red", anchor=S, command=lambda: self.user_answer("False"))
         self.red_button.place(x=200, y=305)
 
         self.window.mainloop()
 
-    def display_question(self):
+    def user_answer(self, value):
         if self.still_has_questions():
-            self.canvas.create_text(70, 70, text=self.next_question(), font='Helvetica 15 bold', fill="black")
-        return
+            self.canvas.delete("all")
+            text = self.check_answer(value)
+            self.score.configure(text=f"Score: {self.score_value}", bg=THEME_COLOR, fg="white", anchor=CENTER)
+            # self.canvas.create_text(150, 90, text=text, font='Helvetica 15 bold', fill="black")
+            # time.sleep(2)
+            self.canvas.create_text(150, 90, text=self.next_question(), font='Helvetica 15 bold', fill="black")
 
     def display_score(self, score):
         self.score.configure(text=f"Score: {score}")
